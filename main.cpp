4de25ed28c8ebe43abc4e3a737a415f8438663e5
Artyom Kolpakov <ddvamp007@gmail.com>
2025-01-08 16:42:07 +0300

    Add configuration

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
+#include "config.hpp"
+
 #include <iostream>
 
-int main() {
+int main(int argc, char **argv) {
+  std::cout << argv[0] << " version " << EXAMPLE_VERSION_MAJOR << '.'
+            << EXAMPLE_VERSION_MINOR << '.' << EXAMPLE_VERSION_PATCH
+            << std::endl;
+
   std::cout << "Hello, world!" << std::endl;
 
   return 0;
